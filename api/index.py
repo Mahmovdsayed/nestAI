5b51dbae59deb32acc0c484eca400a62140b4b5a
Mahmovdsayed <mahmoudsayed3576@gmail.com>
2025-01-08 10:57:30 +0200

    new

--- api/index.py
+++ api/index.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from meta_ai_api import MetaAI
+import time
 
 ai = MetaAI()
 
@@ -19,8 +20,10 @@ app.add_middleware(
 @app.get("/send/")
 async def send_prompt(message: str):
     try:
+        start_time = time.time()
         response = await ai.prompt(message=message)
-        print(response)
+        end_time = time.time()
+        print(f"Execution time: {end_time - start_time} seconds")
         return {"status": "success", "response": response}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
